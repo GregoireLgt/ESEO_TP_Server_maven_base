561014b7453d10497dfcb28d6fb2946734b8ed5e
GregoireLgt <old.times.aka.liebe@gmail.com>
2020-06-21 10:19:58 +0200

    modif mineure v2 jdbcconfiguration

--- ESEO_TP_Server_maven/src/main/java/com/config/JDBCConfiguration.java
+++ ESEO_TP_Server_maven/src/main/java/com/config/JDBCConfiguration.java
@@ -21,7 +21,7 @@ public class JDBCConfiguration {
 		String bDD = "maven";
 		String url = "jdbc:mysql://localhost:3306/" + bDD;
 		String user = "admin";
-		String password = "admin";	// NO SONAR
+		String password = "admin";	// NOSONAR
 		Connection connection = null;
 		// L'essaie de connexion à votre base de donées
 		try {
